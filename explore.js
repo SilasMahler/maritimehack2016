9c84ae2a5f234e4687d8ff4c622f86e911b9be22
Silas Mahler <silas.mahler@fh-dortmund.de>
2016-09-04 04:32:04 +0200

    yo

---
+++ explore.js
@@ -0,0 +1,24 @@
+function init() {
+    var ship = {
+        id: number,
+        name: String,
+        typ: String,
+        length: String,
+        beam: number,
+        manager: String,
+        owner: String,
+        insurer: String,
+        eng_builder: String
+    }
+
+
+    $.get("https://vesseltrip.schroeer.co/vessels", function (data, status) {
+        var content = JSON.parse(data);
+    });
+
+    for(var i = 0; i < content.length ; i++){
+        //TODO befülle tabelle
+    }
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
